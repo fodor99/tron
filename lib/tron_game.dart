d2ff3409a657f2fa7cefe1227ad5e848c7035c88
fodoristvan <fodor.istvan@rfit.hu>
2022-09-23 21:52:22 +0200

    Move orange box with arrow keys

--- lib/tron_game.dart
+++ lib/tron_game.dart
@@ -7,11 +7,14 @@ class TronGame extends FlameGame with KeyboardEvents{
 
   final paint = Paint()..color = Colors.orange;
 
+  double _x = 100.0;
+  double _y = 200.0;
+
   @override
   void render(Canvas canvas) {
     super.render(canvas);
 
-    var rect = const Rect.fromLTWH(100, 200, 100, 100);
+    var rect = Rect.fromLTWH(_x, _y, 100, 100);
     canvas.drawRect(rect, paint);
   }
 
@@ -19,7 +22,20 @@ class TronGame extends FlameGame with KeyboardEvents{
   KeyEventResult onKeyEvent(
       RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
 
-      print(keysPressed);
+      final isKeyDown = event is RawKeyDownEvent;
+
+      if(keysPressed.contains(LogicalKeyboardKey.arrowLeft) && isKeyDown){
+        _x -= 1.0;
+      }else if(keysPressed.contains(LogicalKeyboardKey.arrowRight) && isKeyDown){
+        _x += 1.0;
+      }
+
+      if(keysPressed.contains(LogicalKeyboardKey.arrowUp) && isKeyDown){
+        _y -= 1.0;
+      }else if(keysPressed.contains(LogicalKeyboardKey.arrowDown) && isKeyDown){
+        _y += 1.0;
+      }
+
 
       return KeyEventResult.handled;
   }
