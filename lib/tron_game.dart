5b43a8d07f80a95adab8948c656f2b534820f415
fodoristvan <fodor.istvan@rfit.hu>
2022-09-23 22:14:39 +0200

    Calculate movement with delta (update method)

--- lib/tron_game.dart
+++ lib/tron_game.dart
@@ -1,3 +1,4 @@
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
@@ -7,14 +8,54 @@ class TronGame extends FlameGame with KeyboardEvents{
 
   final paint = Paint()..color = Colors.orange;
 
-  double _x = 100.0;
-  double _y = 200.0;
+
+  bool _goLeft = false;
+  bool _goRight = false;
+  bool _goUp = false;
+  bool _goDown = false;
+
+  static const double _speed = 100.0;
+  static const double _friction = 0.9;
+
+  Vector2 _position = Vector2.zero();
+  Vector2 _movementVector = Vector2.zero();
+
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    final Vector2 inputVector = Vector2.zero();
+
+    if (_goLeft) {
+      inputVector.x -= 1.0;
+    } else if (_goRight) {
+      inputVector.x += 1.0;
+    }
+
+    if (_goUp) {
+      inputVector.y -= 1.0;
+    } else if (_goDown) {
+      inputVector.y += 1.0;
+    }
+
+    if (!inputVector.isZero()) {
+      _movementVector = inputVector;
+
+      _movementVector.normalize();
+      _movementVector *= _speed * dt;
+    } else {
+      _movementVector *= _friction;
+    }
+
+    _position += _movementVector;
+  }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
 
-    var rect = Rect.fromLTWH(_x, _y, 100, 100);
+    var rect = Rect.fromLTWH(_position.x, _position.y, 100, 100);
     canvas.drawRect(rect, paint);
   }
 
@@ -22,18 +63,36 @@ class TronGame extends FlameGame with KeyboardEvents{
   KeyEventResult onKeyEvent(
       RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
 
-      final isKeyDown = event is RawKeyDownEvent;
-
-      if(keysPressed.contains(LogicalKeyboardKey.arrowLeft) && isKeyDown){
-        _x -= 1.0;
-      }else if(keysPressed.contains(LogicalKeyboardKey.arrowRight) && isKeyDown){
-        _x += 1.0;
+      if(keysPressed.contains(LogicalKeyboardKey.arrowLeft) &&
+      event is RawKeyDownEvent){
+        _goLeft = true;
+      }else if(event is RawKeyUpEvent &&
+      event.data.logicalKey == LogicalKeyboardKey.arrowLeft){
+        _goLeft = false;
       }
 
-      if(keysPressed.contains(LogicalKeyboardKey.arrowUp) && isKeyDown){
-        _y -= 1.0;
-      }else if(keysPressed.contains(LogicalKeyboardKey.arrowDown) && isKeyDown){
-        _y += 1.0;
+      if(keysPressed.contains(LogicalKeyboardKey.arrowRight) &&
+          event is RawKeyDownEvent){
+        _goRight = true;
+      }else if(event is RawKeyUpEvent &&
+          event.data.logicalKey == LogicalKeyboardKey.arrowRight){
+        _goRight = false;
+      }
+
+      if(keysPressed.contains(LogicalKeyboardKey.arrowUp) &&
+          event is RawKeyDownEvent){
+        _goUp = true;
+      }else if(event is RawKeyUpEvent &&
+          event.data.logicalKey == LogicalKeyboardKey.arrowUp){
+        _goUp = false;
+      }
+
+      if(keysPressed.contains(LogicalKeyboardKey.arrowDown) &&
+          event is RawKeyDownEvent){
+        _goDown = true;
+      }else if(event is RawKeyUpEvent &&
+          event.data.logicalKey == LogicalKeyboardKey.arrowDown){
+        _goDown = false;
       }
 
 
