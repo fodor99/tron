5ab891b0f18de39ac147d02d8c8cf576a5e05d18
fodoristvan <fodor.istvan@rfit.hu>
2022-09-23 21:45:48 +0200

    Keyboard event detection

--- lib/tron_game.dart
+++ lib/tron_game.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TronGame extends FlameGame with KeyboardEvents{
 
@@ -12,5 +13,14 @@ class TronGame extends FlameGame with KeyboardEvents{
 
     var rect = const Rect.fromLTWH(100, 200, 100, 100);
     canvas.drawRect(rect, paint);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+
+      print(keysPressed);
+
+      return KeyEventResult.handled;
   }
 }
