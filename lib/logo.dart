ea034e64ed9de05c896329df8a17220a1227175d
fodoristvan <fodor.istvan@rfit.hu>
2022-09-24 20:42:23 +0200

    self rotate sprite

--- lib/logo.dart
+++ lib/logo.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flame/components.dart';
 import 'package:tron/tron_game.dart';
 
@@ -7,10 +9,18 @@ class Logo extends SpriteComponent with HasGameRef<TronGame>{
   Future<void> onLoad() async{
     await super.onLoad();
     final gameSize = gameRef.size;
-    position.x = gameSize.x / 2 - (300 / 2) ;
-    position.y = gameSize.y / 2 - (100 / 2) ;
+    position.x = gameSize.x / 2  ;
+    position.y = gameSize.y / 2  ;
     size = Vector2(300.0, 100.0);
     sprite = await Sprite.load('tron.jpg');
     debugMode = true;
+    anchor = Anchor.center;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    angle += 0.25 * dt;
+    angle %= 2 * math.pi;
   }
 }
--- lib/tron_game.dart
+++ lib/tron_game.dart
@@ -1,5 +1,4 @@
 import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
 
 import 'logo.dart';
@@ -12,14 +11,6 @@ class TronGame extends FlameGame{
     add(FpsTextComponent());
     camera.viewport = FixedResolutionViewport(Vector2(size.x, size.y));
     var _logo = Logo();
-    var _rotate = RotateEffect.to(
-        Transform2D.tau,
-      EffectController(
-        duration: 2,
-        infinite: true,
-      )
-    );
-    _logo.add(_rotate);
     add(_logo);
   }
 }
