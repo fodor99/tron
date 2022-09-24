d013c751521e32db6e392cbbf74189b123ae1d50
fodoristvan <fodor.istvan@rfit.hu>
2022-09-24 12:49:13 +0200

    desktop fullscreen setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
 
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'app.dart';
 
-void main() {
+void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     //DESKTOP
+    await DesktopWindow.toggleFullScreen();
   }else if(kIsWeb){
     //WEB
   }else{
