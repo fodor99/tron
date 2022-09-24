63a4bff60191cf23337d089b7083217d6f2591da
fodoristvan <fodor.istvan@rfit.hu>
2022-09-24 21:12:44 +0200

    move logo with on-screen joystick

--- lib/logo.dart
+++ lib/logo.dart
@@ -5,6 +5,10 @@ import 'package:tron/tron_game.dart';
 
 class Logo extends SpriteComponent with HasGameRef<TronGame>{
 
+  final JoystickComponent joystick;
+
+  Logo(this.joystick);
+
   @override
   Future<void> onLoad() async{
     await super.onLoad();
@@ -22,5 +26,18 @@ class Logo extends SpriteComponent with HasGameRef<TronGame>{
     super.update(dt);
     angle += 0.25 * dt;
     angle %= 2 * math.pi;
+
+    if (!joystick.delta.isZero()) {
+      position.add(joystick.relativeDelta * 250 * dt);
+      print(joystick.relativeDelta * 250 * dt);
+    }
+
+
+    // Clamp position of player such that the player sprite does not go outside the screen size.
+    position.clamp(
+      Vector2.zero() + size / 2,
+      gameRef.size - size / 2,
+    );
+
   }
 }
--- lib/tron_game.dart
+++ lib/tron_game.dart
@@ -1,16 +1,39 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:tron/knows_game_size.dart';
 
 import 'logo.dart';
 
-class TronGame extends FlameGame{
+class TronGame extends FlameGame
+    with
+        HasDraggables,
+        HasTappables,
+        KnowsGameSize{
 
   @override
   Future<void> onLoad() async{
     debugMode = true;
     add(FpsTextComponent());
     camera.viewport = FixedResolutionViewport(Vector2(size.x, size.y));
-    var _logo = Logo();
-    add(_logo);
+
+
+    final joystick = JoystickComponent(
+      anchor: Anchor.bottomLeft,
+      position: Vector2(30, size.y - 30),
+      // size: 100,
+      background: CircleComponent(
+        radius: 60,
+        paint: Paint()..color = Colors.white.withOpacity(0.5),
+      ),
+      knob: CircleComponent(radius: 30),
+    );
+    add(joystick);
+
+    final logo = Logo(joystick);
+    add(logo);
+
   }
 }
